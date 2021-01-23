c9c543968e6e7b1c1598511c764cb4ebd38f4f05
WindReiner <44196436+WindReiner@users.noreply.github.com>
2021-01-23 20:53:11 +0800

    finish total frame

--- src/components/sidebar/copyright/index.js
+++ src/components/sidebar/copyright/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './index.module.scss'
 
-const Copyright = ({content}) => {
-    return (<div className={styles.body}>
-        {content}
-    </div>)
+const Copyright = ({ content }) => {
+    return (
+        <div className={styles.body}
+            dangerouslySetInnerHTML={{ __html: content }} />
+    )
 }
 
 export default Copyright
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -4,26 +4,42 @@ import SiteMeta from './sitemeta'
 import Baritem from './baritems'
 import Links from './links/index'
 import Copyright from './copyright/index'
+import { useStaticQuery, graphql } from 'gatsby'
 // import "./index.module.sass"
 
 const Sidebar = () => {
-    console.log(styles);
+    const {site} = useStaticQuery(graphql`
+            query {
+                site {
+                    siteMetadata {
+                      title
+                      description
+                      footerHTML
+                    }
+                }
+            }
+    `);
+
     return (
-    <div className={styles["sideBar"]} id="sideBar" >
-        <div style={{
-            width:"80%",
-            backgroundColor:'green',
-            float:'right',
-            // marginRight: '6%'
-        }}>    
-        <SiteMeta></SiteMeta>
-        <Baritem content="item1"></Baritem>    
-        <Baritem content="item2"></Baritem>
-        <Baritem content="item3"></Baritem>
-        <Links></Links>
-        <Copyright></Copyright>
-        </div>
-    </div>)
+        <div className={styles["sideBar"]} id="sideBar" >
+            <div style={{
+                width: "80%",
+                height: '100%',
+                backgroundColor: 'green',
+                float: 'right',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-around',
+                // marginRight: '6%'
+            }}>
+                <SiteMeta></SiteMeta>
+                <Baritem content="归档"></Baritem>
+                <Baritem content="About"></Baritem>
+                {/* <Baritem content="item3"></Baritem> */}
+                <Links></Links>
+                <Copyright content={site.siteMetadata.footerHTML}></Copyright>
+            </div>
+        </div>)
 }
 
 export default Sidebar
