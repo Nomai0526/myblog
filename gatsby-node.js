bad3e0eb28f2c303a7a70ef0d7aaa1a33dec90df
WindReiner <44196436+WindReiner@users.noreply.github.com>
2021-03-09 15:34:27 +0800

    add fields to database

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,37 +1,79 @@
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
 exports.createPages = async ({ graphql, actions }) => {
-    const { createPages } = actions;
-    // const result = await graphql(
-    //     `
-    //       {
-    //         site {
-    //           siteMetadata {
-    //             dateFormat
-    //           }
-    //         }
-    //         postsRemark: allMarkdownRemark(
-    //           sort: { fields: [fields___date], order: DESC }
-    //           limit: 1000
-    //         ) {
-    //           edges {
-    //             node {
-    //               fields {
-    //                 slug
-    //               }
-    //               frontmatter {
-    //                 title
-    //               }
-    //             }
-    //           }
-    //         }
-    //         tagsGroup: allMarkdownRemark(
-    //           limit: 1000
-    //         ) {
-    //           group(field: frontmatter___tags) {
-    //             fieldValue
-    //           }
-    //         }
-    //       }
-    //     `
-    //   )
-      // console.log(result);
+  const { createPages } = actions;
+  // const result = await graphql(
+  //     `
+  //       {
+  //         site {
+  //           siteMetadata {
+  //             dateFormat
+  //           }
+  //         }
+  //         postsRemark: allMarkdownRemark(
+  //           sort: { fields: [fields___date], order: DESC }
+  //           limit: 1000
+  //         ) {
+  //           edges {
+  //             node {
+  //               fields {
+  //                 slug
+  //               }
+  //               frontmatter {
+  //                 title
+  //               }
+  //             }
+  //           }
+  //         }
+  //         tagsGroup: allMarkdownRemark(
+  //           limit: 1000
+  //         ) {
+  //           group(field: frontmatter___tags) {
+  //             fieldValue
+  //           }
+  //         }
+  //       }
+  //     `
+  //   )
+  // console.log(result);
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === 'MarkdownRemark') {
+    let slug = node.frontmatter.slug || createFilePath({ node, getNode })
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    })
+    try {
+      var date = node.frontmatter.date
+      if (!date) {
+        const filename = node.fileAbsolutePath
+          .split(/.*[\/|\\]/)[1]
+          .split(".")[0]
+        date = new Date(filename.substring(0, 10))
+        if (isNaN(date)) {
+          throw "Invalid Date"
+        }
+      }
+    } catch (error) {
+      console.warn(
+        "Failed to get date from frontmatter or filename, use default date instead.",
+        {
+          slug: slug,
+          filepath: node.fileAbsolutePath,
+          error: error,
+        }
+      )
+      date = new Date("1999-11-26")
+    } finally {
+      createNodeField({
+        node,
+        name: "date",
+        value: date,
+      })
+    }
+  }
 }
